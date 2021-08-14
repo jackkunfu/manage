dbf97be4e51778d74ddcb012794b08b870e09d44
Your Name <you@example.com>
2021-08-14 13:52:54 +0800

    gx

--- src/js/proto.js
+++ src/js/proto.js
@@ -1,74 +1,80 @@
-import axios from 'axios'
+import axios from "axios";
 // import { Loading } from 'element-ui'
-export default function (Vue) {
-  var hrefUrl = location.href
-  // let isNeedNatFree = hrefUrl.indexOf('zhenwu') > -1 || hrefUrl.indexOf('localhost') > -1
-  // var reqBasic = isNeedNatFree ? 'http://q68wtj.natappfree.cc' : `http://${location.hostname}:9000`
-  var reqBasic = 'http://47.99.201.236:9000'
+export default function(Vue) {
+  var hrefUrl = location.href;
+  let isNeedNatFree =
+    hrefUrl.indexOf("zhenwu") > -1 || hrefUrl.indexOf("localhost") > -1;
+  var reqBasic = isNeedNatFree
+    ? "http://q68wtj.natappfree.cc"
+    : `http://${location.hostname}:9000`;
+  // var reqBasic = 'http://47.99.201.236:9000'
   // var reqBasic = 'http://10.100.32.9:9000'
-  Vue.prototype.reqBasic = reqBasic
-  
-  Vue.prototype.goLogin = function () {
-    this.$store.commit('setStoreData', { key: 'isLogin', value: true })
-    this.$router.push({ path: '/' })
-    localStorage.removeItem('MToken')
-    localStorage.removeItem('EVENGFRONTUSER')
-  }
+  Vue.prototype.reqBasic = reqBasic;
 
-  Vue.prototype._fetch = async function (url, data, type) {
-    let loading = this.$loading()
+  Vue.prototype.goLogin = function() {
+    this.$store.commit("setStoreData", { key: "isLogin", value: true });
+    this.$router.push({ path: "/" });
+    localStorage.removeItem("MToken");
+    localStorage.removeItem("EVENGFRONTUSER");
+  };
+
+  Vue.prototype._fetch = async function(url, data, type) {
+    let loading = this.$loading();
     let res = await axios({
-      method: type || 'post',
+      method: type || "post",
       url: this.reqBasic + url, // 直连 后端处理跨域
-      data: type && type === 'get' ? {} : data,
+      data: type && type === "get" ? {} : data,
       // params: type && type === 'get' ? data : {},
       params: data,
-      dataType: 'json',
+      dataType: "json",
       headers: {
-        'Content-Type': 'application/json',
-        token: localStorage.MToken || ''
+        "Content-Type": "application/json",
+        token: localStorage.MToken || ""
       }
-    })
-    loading.close()
-    let result = res.data
+    });
+    loading.close();
+    let result = res.data;
     if (result) {
       if (result.code === -1) {
         if (result.msg) {
-          if (result.msg.indexOf('token') > -1) this.goLogin()
-          else this._messageTip(result.msg)
-        } else this._messageTip('请求失败')
-      } else return result || {}
-    } else throw new Error('请求失败')
-  }
+          if (result.msg.indexOf("token") > -1) this.goLogin();
+          else this._messageTip(result.msg);
+        } else this._messageTip("请求失败");
+      } else return result || {};
+    } else throw new Error("请求失败");
+  };
 
   // Vue.prototype.$loading = function (url, data, type) {
   //   let loading = Loading.service()
   //   return loading
   // }
 
-  Vue.prototype.pageBack = function () {
-    window.history.go(-1)
-  }
+  Vue.prototype.pageBack = function() {
+    window.history.go(-1);
+  };
 
-  Vue.prototype._goUrl = function (path, data = {}) {
+  Vue.prototype._goUrl = function(path, data = {}) {
     this.$router.push({
-      path: path, query: data.query || {}, params: data.params
-    })
-  }
+      path: path,
+      query: data.query || {},
+      params: data.params
+    });
+  };
 
-  Vue.prototype._messageTip = function (message = '', type) {
+  Vue.prototype._messageTip = function(message = "", type) {
     this.$message({
-      message, type: type === 1 ? 'success' : type === 2 ? 'warn' : 'error'
-    })
-  }
+      message,
+      type: type === 1 ? "success" : type === 2 ? "warn" : "error"
+    });
+  };
 
-  Vue.prototype._confirm = function (str, fn) {
-    return this.$confirm(str, '提示', {
-      confirmButtonText: '确定',
-      cancelButtonText: '取消',
-      type: 'warning'
+  Vue.prototype._confirm = function(str, fn) {
+    return this.$confirm(str, "提示", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: "warning"
     }).then(_ => {
-      return _
-    })
-  }
+      return _;
+    });
+  };
 }
